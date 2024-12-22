b698ea69cfef9c63000aa005244f3f0fb6f11f16
Triysle <triyslebrin@gmail.com>
2024-12-22 03:34:59 -0800

    Initial commit

---
+++ player.gd
@@ -0,0 +1,9 @@
+extends RigidBody2D
+
+var impulse_force : int = 1200
+
+func _process(_delta):
+	if Input.is_action_just_pressed("click"):
+		apply_central_impulse(Vector2.UP * impulse_force)
+		
+	$Particles.position = Vector2(position.x - 40, position.y - 10)
